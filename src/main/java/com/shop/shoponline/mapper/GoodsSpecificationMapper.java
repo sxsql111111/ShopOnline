0811fb2fb57862d667d5435d75c424e9cefeb82e
sx <3049816743@qq.com>
2023-11-08 08:24:07 +0800

    搭建项目基础框架-最终版本

---
+++ src/main/java/com/shop/shoponline/mapper/GoodsSpecificationMapper.java
@@ -0,0 +1,16 @@
+package com.shop.shoponline.mapper;
+
+import com.shop.shoponline.entity.GoodsSpecification;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author sx
+ * @since 2023-11-07
+ */
+public interface GoodsSpecificationMapper extends BaseMapper<GoodsSpecification> {
+
+}
---
+++ src/main/java/com/shop/shoponline/mapper/IndexCarouselMapper.java
@@ -0,0 +1,16 @@
+package com.shop.shoponline.mapper;
+
+import com.shop.shoponline.entity.IndexCarousel;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author sx
+ * @since 2023-11-07
+ */
+public interface IndexCarouselMapper extends BaseMapper<IndexCarousel> {
+
+}
